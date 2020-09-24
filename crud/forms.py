8ebfb8b66dfc9ad2d998bc946c0a49bbd6062eba
Jhern603 <Jhern603@fiu.edu>
2020-09-24 17:09:45 -0400

    Dynamic Active links in the header Added login and register link on header BUGS: UserMixIn/Sessions not implemented properly

--- crud/forms.py
+++ crud/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
 from flask import flash
-from wtforms import StringField, PasswordField, TextAreaField, SelectField, HiddenField
+from wtforms import StringField, PasswordField, TextAreaField, SelectField, HiddenField, BooleanField
 from wtforms.validators import ValidationError, Length, EqualTo, DataRequired
 from crud.models import users
 
-# Login form manager
-class registerForm(FlaskForm):
+# Register form manager
+
+
+class register_form(FlaskForm):
     id = HiddenField('id')
     email = StringField('E-mail', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
@@ -16,20 +18,33 @@ class registerForm(FlaskForm):
     def validate_username(self, username):
         user = users.query.filter_by(username=username.data).first()
         if user:
-           flash('This user already exists.')
+            flash('This user already exists.')
+
+# Login form manager
+
+
+class login_form(FlaskForm):
+    id = HiddenField('id')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me.', validators=[DataRequired()])
 
 
 # Edit User form manager
-class editUserForm(FlaskForm):
+class edit_user_form(FlaskForm):
     id = HiddenField('id')
     email = StringField('E-mail', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
-    username = StringField('Username', validators=[DataRequired(), Length(min=4,max=25)])
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=4, max=25)])
     password = PasswordField('Password')
     phone = StringField('Phone Number', validators=[DataRequired()])
 
 # Post form manager
-class postForm(FlaskForm):
+
+
+class post_form(FlaskForm):
+    id = HiddenField('id')
     title = StringField('Title', validators=[DataRequired()])
     author = StringField('Author')
     content = StringField('Post content', validators=[DataRequired()])
--- crud/routes.py
+++ crud/routes.py
@@ -1,30 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from crud import db, app, bcrypt
 from crud.models import postdb, users, main
-from crud.forms import registerForm, postForm, editUserForm
+from crud.forms import register_form, post_form, edit_user_form, login_form
+from flask_login import login_user
 
 
 @app.route('/')
 def index():
-    login = registerForm()
-    return render_template('index.html', loginform=login)
+    register = register_form()
+    login = login_form()
+    return render_template('index.html', registerform=register, loginform=login)
 
-#Forum Pages
+
+# Forum Pages
 @app.route('/posts/',)
 def posts():
-    postData = postdb.query.all()
-    postFormInput = postForm()
-    return render_template('posts.html', postData=postData, post=postFormInput)
+    post_data = postdb.query.all()
+    post_form_input = post_form()
+    return render_template('posts.html', postData=post_data, postForm=post_form_input)
+
 
 @app.route('/posts/new', methods=['POST'])
 def new():
+    post = post_form()
     if request.method == 'POST':
-        title = postForm.title.data
-        author = postForm.author.data
-        content = postForm.content.data
+        title = post.title.data
+        author = post.author.data
+        content = post.content.data
 
-        newPostdb = postdb(title, author, content)
-        db.session.add(newPostdb)
+        new_postdb = postdb(title, author, content)
+        db.session.add(new_postdb)
         db.session.commit()
 
         flash('"' + title + '"' + " was posted successfully!")
@@ -34,16 +39,18 @@ def new():
 
 @app.route('/posts/editPost', methods=['GET', 'POST'])
 def editPost():
+    post = post_form()
     if request.method == 'POST':
-        data = postdb.query.get(request.form.get('id'))
-        data.title = request.form['title']
-        data.author = request.form['author']
-        data.content = request.form['content']
+        data = postdb.query.get(post.id.data)
+        data.title = post.title.data
+        data.author = post.author.data
+        data.content = post.content.data
         db.session.commit()
 
         flash('"' + data.title + '"' + " edited successfully!")
 
         return redirect(url_for('posts'))
+
 
 @app.route('/posts/deletepost/<id>/', methods=['GET', 'POST'])
 def deletepost(id):
@@ -55,40 +62,54 @@ def deletepost(id):
 
     return redirect(url_for('posts'))
 
-#User Management pages
-@app.route('/login/', methods=['POST'])
+
+# User Management pages
+@app.route('/login', methods=['POST'])
 def login():
-    return render_template('index.html')
+    login = login_form()
+    user = users.query.filter_by(username=login.username.data).first()
+    if user and bcrypt.check_password_hash(user.password, login.password.data):
+        #login_user(user, remember=login.remember.data)
+        return redirect(url_for('manageUsers'))
+    else:
+        flash("Failed to log you in. Please try again.")
+    return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['POST'])
 def register():
-    register = registerForm()
+    register = register_form()
     if register.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(register.password.data).decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(
+            register.password.data).decode('utf-8')
         password = hashed_password
-        newUserdb = users(username=register.username.data, email=register.email.data, name=register.name.data, number=register.phone.data, password=password)
+        newUserdb = users(username=register.username.data, email=register.email.data,
+                          name=register.name.data, number=register.phone.data, password=password)
         db.session.add(newUserdb)
         db.session.commit()
         flash('You have successfully registered!')
 
     return redirect(url_for('index'))
 
+
 @app.route('/manageUsers/')
 def manageUsers():
-    login = registerForm()
+    login = register_form()
     userData = users.query.all()
-    edit = editUserForm()
+    edit = edit_user_form()
 
     return render_template('manageUsers.html', user=userData, loginform=login, editUser=edit)
+
 
 @app.route('/manageUsers/insert', methods=['POST'])
 def insert():
     if request.method == 'POST':
-        register = registerForm()
+        register = register_form()
         username = register.username.data
         email = register.email.data
         name = register.name.data
-        hashed_password = bcrypt.generate_password_hash(register.password.data).decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(
+            register.password.data).decode('utf-8')
         password = hashed_password
         number = register.phone.data
 
@@ -104,7 +125,7 @@ def insert():
 @app.route('/manageUsers/edit/', methods=['GET', 'POST'])
 def edit():
     if request.method == 'POST':
-        edit = editUserForm()
+        edit = edit_user_form()
         data = users.query.get(edit.id.data)
         data.username = edit.username.data
         data.name = edit.name.data
